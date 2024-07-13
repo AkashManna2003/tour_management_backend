855460237a1bd6b377cb14955c6d455ed4e00843
Akash Manna <112533557+AkashManna2003@users.noreply.github.com>
2024-07-13 22:36:13 +0530

    Update verifyToken.js

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -3,8 +3,9 @@ import jwt from 'jsonwebtoken'
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.accessToken
 
-    if(token){
+    if(!token){
         return res.status(401).json({success:false, message:'You are not authorised'})
+        console.log('TOKEN: ',token);
     }
 
     //if token exists then verify the token
